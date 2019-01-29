2ef872f000fdb642d229dda03abd912b3fc38c53
Georgi Dikov <gvdikov@gmail.com>
2019-01-29 10:57:26 +0100

    add most chunks of the gameplay, not tested yet

--- snake/environment.py
+++ snake/environment.py
@@ -29,6 +29,8 @@ class Coordinate(object):
     def __add__(self, other):
         if isinstance(other, int):
             return Coordinate(self.x + other, self.y + other)
+        if isinstance(other, tuple) and len(other) == 2:
+            return Coordinate(self.x + other[0], self.y + other[1])
         return Coordinate(self.x + other.x, self.y + other.y)
 
     def __neg__(self):
@@ -37,6 +39,8 @@ class Coordinate(object):
     def __sub__(self, other):
         if isinstance(other, int):
             return Coordinate(self.x - other, self.y - other)
+        if isinstance(other, tuple) and len(other) == 2:
+            return Coordinate(self.x - other[0], self.y - other[1])
         return Coordinate(self.x - other.x, self.y - other.y)
 
     def __mul__(self, other):
@@ -54,7 +58,7 @@ def close_bounds(func):
         new_obj = func(self, *args)
         new_obj.x %= self._max_x
         new_obj.y %= self._max_y
-        return new_obj
+        return TorusCoordinate(new_obj.x, new_obj.y, self._max_x, self._max_y)
     return call_and_moddiv
 
 
@@ -81,16 +85,20 @@ class TorusCoordinate(Coordinate):
         return super(TorusCoordinate, self).__floordiv__(other)
 
 
+Coord = Coordinate
+TCoord = TorusCoordinate
+
+
 class Pixel(object):
     def __init__(self, coord, rgb=(0, 0, 0)):
         self._coord = coord
         self._rgb = rgb
 
     def __repr__(self):
-        return f"[{self.position}, RGB: {self.rgb}]"
+        return f"[{self.coordinate}, RGB: {self.rgb}]"
 
     @property
-    def position(self):
+    def coordinate(self):
         return self._coord
 
     @property
@@ -104,11 +112,69 @@ class Pixel(object):
         self._rgb = rgb
 
 
+class Fruit:
+    def __init__(self, coord, col):
+        self._color = col
+        self._body = Pixel(coord, col)
+
+    def respawn(self, free_pos):
+        if len(free_pos) == 0:
+            return
+        new_coord = rand_choice(free_pos)
+        self._body = Pixel(new_coord, self._color)
+
+    @property
+    def position(self):
+        return self._body.coordinate
+
+
+class Snake:
+    def __init__(self, size, init_pos):
+        self.size = size
+        self.body = deque()
+        self.body_wo_head = set()
+        self.body.append(init_pos)
+        for i in range(size - 1):
+            self.body.append(init_pos - [0, i])
+
+    @property
+    def head(self):
+        return self.body[-1]
+
+    @property
+    def tail(self):
+        return self.body[0]
+
+    def move(self, action, grow=False):
+        if action == 'H':
+            return
+        if not grow:
+            tail = self.body.popleft()
+            self.body_wo_head.remove(tail)
+        else:
+            self.size += 1
+        if action == 'U':
+            new_head = self.head + (0, 1)
+        elif action == 'D':
+            new_head = self.head - (0, 1)
+        elif action == 'L':
+            new_head = self.head + (1, 0)
+        elif action == 'R':
+            new_head = self.head - (1, 0)
+        else:
+            raise ValueError(f"Unknown action {action}.")
+        self.body_wo_head.add(self.head)
+        self.body.append(new_head)
+
+    def is_selfbiting(self):
+        return self.head in self.body_wo_head
+
+
 class Board(object):
     def __init__(self, n_rows, n_cols):
         self._n_rows = n_rows
         self._n_cols = n_cols
-        self._pixels = [Pixel(TorusCoordinate(i, j, n_rows, n_cols), (0, 0, 0))
+        self._pixels = [Pixel(TCoord(i, j, n_rows, n_cols), (0, 0, 0))
                         for i in range(n_rows) for j in range(n_cols)]
 
     @property
@@ -145,33 +211,6 @@ class Board(object):
             yield p.rgb
 
 
-class Snake:
-    def __init__(self, size, init_pos):
-        self.size = size
-        self.body = deque()
-        self.body.append(init_pos)
-        for i in range(size - 1):
-            self.body.append(init_pos - [0, i])
-
-    def move(self, a):
-        b = list()
-        b.remove()
-
-
-class Fruit:
-    def __init__(self, coord, col):
-        self._color = col
-        self._body = Pixel(coord, col)
-
-    def respawn(self, free_pos):
-        new_coord = rand_choice(free_pos)
-        self._body = Pixel(new_coord, self._color)
-
-    @property
-    def position(self):
-        return self._body.position
-
-
 class Environment:
     def __init__(self, size):
         if isinstance(size, int):
@@ -179,12 +218,13 @@ class Environment:
         self.world_size = size
         self.snake_size = 1
         self.snake_color = (255, 255, 255)
-        self.snake_pos_init = TorusCoordinate(size[0], size[1], size[0], size[1]) // 2
+        self.snake_pos_init = TCoord(size[0], size[1], size[0], size[1]) // 2
         self.snake = None
 
         self.fruit_size = 1
         self.fruit_color = (255, 0, 0)
         self.fruit = Fruit((0, 0), self.fruit_color)
+        self.fruit_prev_pos = TCoord(0, 0, size[0], size[1])
 
         self.empty_color = (0, 0, 0)
 
@@ -196,18 +236,31 @@ class Environment:
     def get_empty_coords(self):
         return [p.position for p in self.board if p.rgb == self.empty_color]
 
-    def clear(self):
-        self.board.set_all(self.empty_color)
-
     @property
     def state(self):
         return list(self.board)
 
     def reset(self):
-        self.clear()
+        self.board.set_all(self.empty_color)
         self.score = 0
         self.snake = Snake(1, self.snake_pos_init)
         self.fruit.respawn(self.get_empty_coords())
+        self.fruit_prev_pos = self.fruit.position
+        return 'P', self.score
 
     def update_state(self, action):
-        pass
+        grow_snake = False
+        if self.snake.tail == self.fruit_prev_pos:
+            grow_snake = True
+            self.fruit_prev_pos = self.fruit.position
+        self.snake.move(action, grow_snake)
+        if self.snake.head == self.fruit.position:
+            self.score += 1
+            self.fruit.respawn(free_pos=self.get_empty_coords())
+        # check for a win
+        if self.snake.size == self.board.n_cols * self.board.n_rows:
+            return 'W', self.score
+        # check for a lost
+        if self.snake.is_selfbiting():
+            return 'L', self.score
+        return 'P', self.score
